edbd686bc33dbc938b1e93994297c8aea251fc73
Tuan Nguyen <tuan.nat915@gmail.com>
2024-04-16 09:53:53 -0400

    fixed bug

--- components/shared/header/MobileNav.jsx
+++ components/shared/header/MobileNav.jsx
@@ -1,15 +1,7 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { AlignJustify } from "lucide-react"
 import {
   Sheet,
-  SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
 import Logo from "./Logo"
--- components/shared/hero/Hero.jsx
+++ components/shared/hero/Hero.jsx
@@ -5,7 +5,7 @@ import HeroImg from "./HeroImg"
 
 const Hero = () => {
   return (
-    <section className="flex min-h-[90vh] flex-col items-center justify-between bg-hero bg-cover bg-bottom bg-no-repeat py-10 dark:bg-none xl:py-24 xl:pt-16">
+    <section className="mt-12 flex min-h-[90vh] flex-col items-center justify-between bg-hero bg-cover bg-bottom bg-no-repeat py-10 dark:bg-none lg:mt-32 xl:py-24 xl:pt-16">
       <div className="container mx-auto mt-5 xl:mt-16">
         <div className="flex justify-between gap-8">
           <div className="mx-auto flex flex-col justify-center text-center lg:mx-0 lg:text-left">
@@ -21,7 +21,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="bottom-44 left-1/2 hidden animate-bounce lg:bottom-1 lg:flex">
+      <div className="hidden animate-bounce lg:flex md:pb-44">
         <RiArrowDownSLine className="text-3xl text-primary" size={40} />
       </div>
     </section>
